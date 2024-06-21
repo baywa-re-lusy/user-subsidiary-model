71aa148aa3adf542b0e7c11af7222476db03efbc
Pascal Paulis <ppaulis@gmail.com>
2024-06-21 09:52:32 +0200

    bugfix
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/UserSubsidiaryModel/UserInterface.php
+++ src/UserSubsidiaryModel/UserInterface.php
@@ -12,8 +12,8 @@ interface UserInterface
     public function setEmail(string $email): UserInterface;
     public function getEmailVerified(): bool;
     public function setEmailVerified(bool $emailVerified): UserInterface;
-    public function getCreated(): ?\DateTime;
-    public function setCreated(?\DateTime $created): UserInterface;
+    public function getCreated(): \DateTime;
+    public function setCreated(\DateTime $created): UserInterface;
     public function getSubsidiaryIds(): array;
     public function setSubsidiaryIds(array $subsidiaryIds): UserInterface;
     public function getRoles(): array;
